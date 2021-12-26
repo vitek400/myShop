bc18a7f27c30483cfd11e16cfb45abe9fcf39aae
Lidiya <lima07@mail.ru>
2021-12-26 23:33:37 +0400

    Создала файл Storage

---
+++ src/Ru/retail/Storage.java
@@ -0,0 +1,14 @@
+package Ru.retail;
+
+public class Storage {
+    public static void main(String[] args) {
+        int n = 10;
+        while (n <= 50) {
+            n++;
+
+            System.out.println("Товар на складе в достаточном количестве");
+
+
+        }
+    }
+}
