b31d58ab8e0adf4f883ef0b9b58c2ce942a6bbe7
Lidiya <lima07@mail.ru>
2021-12-26 23:25:41 +0400

    Добавила файл Main

---
+++ src/Ru/retail/Main.java
@@ -0,0 +1,34 @@
+package Ru.retail;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean productPaidFor= true;
+        boolean productNotPaid=false;
+
+        if(productPaidFor()) {
+            System.out.println("При доставке товара отдаем кассовый чек");
+        }
+        else if(productNotPaid()) {
+            System.out.println("При доставке товара принимаем оплату и отдаем кассовый чек");
+
+        }
+        else {
+            System.out.println("Не отдаем товар покупателю");
+        }
+    }
+
+    private static boolean productNotPaid() {
+
+        return false;
+    }
+
+    private static boolean productPaidFor() {
+
+        return true;
+    }
+
+}
+
+
+
+
