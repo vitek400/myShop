ed57db52f2420b60c1c97aeb3022947b250fab46
Lidiya <lima07@mail.ru>
2021-12-27 00:55:42 +0400

    Декоративные правки

--- src/Ru/retail/discounts/Discounts.java
+++ src/Ru/retail/discounts/Discounts.java
@@ -1,17 +1,14 @@
 package Ru.retail.discounts;
 
-public class Discounts{
+public class Discounts {
     public static void main(String[] args) {
         int n=10;//сумма скидки
         do{
             System.out.println("Выдаем подарок");
             n--;
         } while (n>0);
-
     }
-
-
-    }
+}
 
 
 
