a9336ca0229a96ad820ffd1bec199858d855f32f
Lidiya <lima07@mail.ru>
2021-12-26 23:36:12 +0400

    Добавила файл Discounts

---
+++ src/Ru/retail/discounts/Discounts.java
@@ -0,0 +1,32 @@
+package Ru.retail.discounts;
+
+public class Discounts{
+    public static void main(String[] args) {
+        int n=10;//сумма скидки
+        do{
+            System.out.println("Выдаем подарок");
+            n--;
+        } while (n>0);
+
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
