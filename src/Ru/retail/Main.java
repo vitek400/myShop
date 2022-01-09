54b8ad7655c06a7f0a89999c1527ebf78a9479f5
Lidiya <lima07@mail.ru>
2022-01-10 01:08:09 +0400

    массивы с примерами for/foreach

--- src/Ru/retail/Main.java
+++ src/Ru/retail/Main.java
@@ -1,7 +1,30 @@
 package Ru.retail;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int [] stock = new int[10]; //запас товара на складе
+        for(int i=0; i<10; i++){
+            stock[i] = i+1;
+            System.out.println(stock[i]);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   //      boolean productPaidFor = true;
  //       boolean productNotPaid = false;
   //      if(productPaidFor()) {
@@ -10,8 +33,8 @@ public class Main {
  //           System.out.println("При доставке товара принимаем оплату и отдаем кассовый чек");
  //       } else {
  //           System.out.println("Не отдаем товар покупателю");
-        }
-    }
+
+
   //  private static boolean productNotPaid() {
  //       return false;
  //   }
