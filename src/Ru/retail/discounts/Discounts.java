cb8987fe3b9a881b4024e0561a04617fa156373f
Lidiya <lima07@mail.ru>
2022-01-21 20:57:56 +0400

    добавила методы и изменила классы на абстрактные

--- src/Ru/retail/discounts/Discounts.java
+++ src/Ru/retail/discounts/Discounts.java
@@ -1,12 +1,44 @@
 package Ru.retail.discounts;
 
-public class Discounts {
+public abstract class Discounts {
     private int idProduct;
     private int dateOfStart;
     private int dateOfTheEnd;
     private String kindOfDiscount;
 
-    public Discounts (int idProduct, int dateOfStart,int dateOfTheEnd, String kindOfDiscount){
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public int getDateOfStart() {
+        return dateOfStart;
+    }
+
+    public void setDateOfStart(int dateOfStart) {
+        this.dateOfStart = dateOfStart;
+    }
+
+    public int getDateOfTheEnd() {
+        return dateOfTheEnd;
+    }
+
+    public void setDateOfTheEnd(int dateOfTheEnd) {
+        this.dateOfTheEnd = dateOfTheEnd;
+    }
+
+    public String getKindOfDiscount() {
+        return kindOfDiscount;
+    }
+
+    public void setKindOfDiscount(String kindOfDiscount) {
+        this.kindOfDiscount = kindOfDiscount;
+    }
+
+    public Discounts (int idProduct, int dateOfStart, int dateOfTheEnd, String kindOfDiscount){
         this.idProduct = idProduct;
         this.dateOfStart = dateOfStart;
         this.dateOfTheEnd = dateOfTheEnd;
