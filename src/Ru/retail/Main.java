ff8fd8624ce0b1e54138a4ef90432bb18fa211f5
vitek <vitek@Topaz1.local>
2022-02-10 15:17:51 +0300

    Клиенты теперь хранятся в коллекции

--- src/Ru/retail/Main.java
+++ src/Ru/retail/Main.java
@@ -9,28 +9,32 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Client client1 = new Client("Петр", "258741", "741852");
-        Client client2 = new Client("Иванко", "258571", "123654");
-        Client client3 = new Client("Валентин", "274181", "741852");
-        Client client4 = new Client("Денис", "528964", "789624");
-        Client client5 = new Client("Сергей", "первый", "201");
-        List<Client> clients = List.of(client1, client2, client3, client4, client5);
+        List<Client> clients = new ArrayList();
+        client.append( new Client("Петр",     "258741", "741852") );
+        client.append( new Client("Иванко",   "258571", "123654") );
+        client.append( new Client("Валентин", "274181", "741852") );
+        client.append( new Client("Денис",    "528964", "789624") );
+        client.append( new Client("Сергей",   "первый", "201"   ) );
+        
         Uneatable toy = new Uneatable("Радиоуправляемый вертолет","Пхеньян","73023","3000","8547","склад");
-        Uneatable washingPowder = new Uneatable("Персил","Мехико","74258","680","369874","склад");
-        Order order = new Order("радиоуправляемый вертолет, персил","Одесса","Сергей","безналичная оплата","03.03.2022","г.Одесса,ул.Школьная,д.2");
         toy.acceptProduct("склад");
         toy.setBarcode("8547");
         toy.setPrice("3000");
         toy.setState("В наличии");
+        
+        Uneatable washingPowder = new Uneatable("Персил","Мехико","74258","680","369874","склад");
         washingPowder.acceptProduct("склад");
         washingPowder.setBarcode("369874");
         washingPowder.setPrice("680");
         washingPowder.setState("в наличии");
-        client5.putInBasket(toy);
-        client5.putInBasket(washingPowder);
-        client5.setOrder("первый");
-        client5.payOrder("Одесса", "Сергей", "оплата по карте");
-        client5.accumDiscount();
+        
+        client[5].putInBasket(toy);
+        client[5].putInBasket(washingPowder);
+        client[5].setOrder("первый");
+        client[5].payOrder("Одесса", "Сергей", "оплата по карте");
+        client[5].accumDiscount();
+        
+        Order order = new Order("радиоуправляемый вертолет, персил","Одесса","Сергей","безналичная оплата","03.03.2022","г.Одесса,ул.Школьная,д.2");
         order.transferDelivery();
         order.delivery();
     }
