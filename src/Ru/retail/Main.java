e6133c03c4cf8532087330399e84edd10bf1da00
vitek <vitek@Topaz1.local>
2022-02-10 16:26:55 +0300

    Исправление ошибок

--- src/Ru/retail/Main.java
+++ src/Ru/retail/Main.java
@@ -9,12 +9,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Client> clients = new ArrayList();
-        client.add( new Client("Петр",     "258741", "741852") );
-        client.add( new Client("Иванко",   "258571", "123654") );
-        client.add( new Client("Валентин", "274181", "741852") );
-        client.add( new Client("Денис",    "528964", "789624") );
-        client.add( new Client("Сергей",   "первый", "201"   ) );
+        List<Client> clients = new ArrayList<Client>();
+        clients.add( new Client("Петр",     "258741", "741852") );
+        clients.add( new Client("Иванко",   "258571", "123654") );
+        clients.add( new Client("Валентин", "274181", "741852") );
+        clients.add( new Client("Денис",    "528964", "789624") );
+        clients.add( new Client("Сергей",   "первый", "201"   ) );
         
         Uneatable toy = new Uneatable("Радиоуправляемый вертолет","Пхеньян","73023","3000","8547","склад");
         toy.acceptProduct("склад");
@@ -28,11 +28,11 @@ public class Main {
         washingPowder.setPrice("680");
         washingPowder.setState("в наличии");
         
-        client[5].putInBasket(toy);
-        client[5].putInBasket(washingPowder);
-        client[5].setOrder("первый");
-        client[5].payOrder("Одесса", "Сергей", "оплата по карте");
-        client[5].accumDiscount();
+        clients.get(4).putInBasket(toy);
+        clients.get(4).putInBasket(washingPowder);
+        clients.get(4).setOrder("первый");
+        clients.get(4).payOrder("Одесса", "Сергей", "оплата по карте");
+        clients.get(4).accumDiscount();
         
         Order order = new Order("радиоуправляемый вертолет, персил","Одесса","Сергей","безналичная оплата","03.03.2022","г.Одесса,ул.Школьная,д.2");
         order.transferDelivery();
